4f30436ca9282c0b31c4410583d6456c3d133262
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-07-10 13:10:19 -0500

    init perlin walk start

---
+++ PerlinWalk/PerlinWalker.js
@@ -0,0 +1,21 @@
+class Walker {
+    constructor(){
+        this.tx = 0;
+        this.ty = 10000;
+    }
+
+    step(){
+        this.x = map(noise(this.tx), 0, 1, 0, width);
+        this.y = map(noise(this.ty), 0, 1, 0, height);
+
+        this.tx += 0.01;
+        this.ty += 0.01;
+
+    }
+
+
+    show(){
+        
+
+    }
+}
