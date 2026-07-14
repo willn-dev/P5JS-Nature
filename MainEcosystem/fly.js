f186c12527bd52401b9737a5676e265f00f116cd
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-07-14 17:44:10 -0500

    Added a fly as the first element of my ecosystem. implements a switch statement for behavior, so cases can easily be added for the flys current state

---
+++ MainEcosystem/fly.js
@@ -0,0 +1,69 @@
+//making a fly that lands and flies again. 
+
+
+class Fly{
+
+    constructor() {
+        this.x = width / 2; 
+        this.y = height / 2;
+        this.perlin_x = 0;
+        this.perlin_y = 100000;
+        this.flight_time = 0; // initially I put this within movement, but I dont want it to reset each time
+        this.rest_time = 0;
+
+        this.state = 'flying';
+        //eventually there will be hunger, health, thirst etc
+    }
+
+    movement(){
+
+      switch(this.state) {
+
+        //----------------------------FLIGHT LOGIC--------------------------
+        
+        case 'flying':
+          if (this.flight_time > 800){ //can land
+
+            let chance_stop = random(1);
+            if (chance_stop < 0.05){ //landing
+              this.flight_time = 0;
+              this.state = 'rest';
+              break;
+            }
+
+            } else{ //still flying
+              this.x = map(noise(this.perlin_x), 0, 1, 0, width);
+              this.y = map(noise(this.perlin_y), 0, 1, 0, height);
+
+              //increments
+              this.flight_time += 1;
+              this.perlin_x += 0.01;
+              this.perlin_y += 0.01;
+              break;
+            };
+        //-------------------------REST------------------------------------
+        
+        case 'rest':
+          if (this.rest_time > 800){
+            let get_up = random(1);
+
+            if (get_up < 0.05){
+              this.rest_time = 0;
+              this.state = 'flying';
+              break;
+            }
+          } 
+          else{ 
+            this.rest_time += 1;
+            break;
+          }
+      }
+    }
+
+    show(){
+      background(220);
+      stroke(0);
+      circle(this.x, this.y, 50);
+    }
+
+}
--- MainEcosystem/sketch.js
+++ MainEcosystem/sketch.js
@@ -1,7 +1,12 @@
+let fly;
+
 function setup() {
     createCanvas(800, 600);
+    background(220);
+    fly = new Fly();
 }
 
 function draw() {
-    background(220);
+    fly.show();
+    fly.movement();
 }
