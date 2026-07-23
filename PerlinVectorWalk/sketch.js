431d406b140f373f06fcd5d23202470a156e7b5e
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-07-23 08:01:09 -0500

    change count to 500

--- PerlinVectorWalk/sketch.js
+++ PerlinVectorWalk/sketch.js
@@ -2,7 +2,7 @@ let walkerarr = [];
 
 function setup() {
     createCanvas(800, 600);
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 500; i++){
         let x = random(0,800);
         let y = random(0,600);
         walkerarr.push(new Walker(x,y));
